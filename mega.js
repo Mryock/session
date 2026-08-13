d99848ecb4e64ff0a8c9ce36a1cbd0b2778b2620
Mryock <linmaximilian1@yahoo.com>
2026-08-13 15:43:34 +0300

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,7 +2,7 @@ import { Storage } from 'megajs';
 
 const auth = {
     email: process.env.MEGA_EMAIL || 'itz.3.kid@gmail.com',
-    password: process.env.MEGA_PASSWORD || 'Clare2008@2008',
+    password: process.env.MEGA_PASSWORD || 'Clare2008/',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
